6b8c98bed38c89b2a06439d2d2e74ea4baa3c1e3
Henry Maestu <henry.maestu@gmail.com>
2017-02-22 00:56:18 +0200

    Added Google Analytics and jQuery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation();
+
+$(document).ready(function() {
+    $(".menu a").click(function() {
+       ga('send', 'event', 'menu-links', 'Click', $(this).attr("href")); 
+    });
+});
