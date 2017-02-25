b61d6f4a10ad8ae2d007eff291e26c545cfe1e74
Henry Maestu <henry.maestu@gmail.com>
2017-02-25 02:18:03 +0200

    test

--- js/app.js
+++ js/app.js
@@ -2,18 +2,6 @@ $(document).ready(function() {
     $(".menu a").click(function() {
        ga('send', 'event', 'menu-links', 'Click', $(this).attr("href")); 
     });
-    $('#container').imagesLoaded()
-    .always( function( instance ) {
-        console.log('all images loaded');
-    })
-    .done( function( instance ) {
-        console.log('all images successfully loaded');
-    })
-    .fail( function() {
-        console.log('all images loaded, at least one is broken');
-    })
-    .progress( function( instance, image ) {
-        var result = image.isLoaded ? 'loaded' : 'broken';
-        console.log( 'image is ' + result + ' for ' + image.img.src );
-    });
+    
+     $('#loading_wrap').remove();
 });
