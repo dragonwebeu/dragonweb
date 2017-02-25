fa8e4b58e6e0ed0f7ef3fff69cbc7c189cba9c3f
Henry Maestu <henry.maestu@gmail.com>
2017-02-25 02:23:57 +0200

    jQuery

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $(".menu a").click(function() {
        ga('send', 'event', 'menu-links', 'Click', $(this).attr("href")); 
-    });
-    $(window).load(function() {
-        $('.uil-ring-css').remove();
-    });
+    });    
+});
+
+$(window).load(function() {
+    $('.uil-ring-css').remove();
 });
 
