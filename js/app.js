1dbf758e4b7e60a4a8418d149fea97438eb5c60e
Henry Maestu <henry.maestu@gmail.com>
2017-02-25 14:58:22 +0200

    scrolllock

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(window).on('load' ,function(e){
     $('.modal').remove();    
     $(".content-top.animated").addClass("fadeIn");
     $(".web-design img.animated").addClass("slideInUp");    
-    $("body").on("scroll", function(e){
+    $(document).on("scroll", function(e){
         console.log(this);
         e.preventDefault();
     });
