0a0e8d6b90801d22e005ad42f51b38ff84e35820
Henry Maestu <henry.maestu@gmail.com>
2017-02-25 14:54:49 +0200

    test

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@
 $(window).on('load' ,function(e){    
     $('.modal').remove();    
     $(".content-top.animated").addClass("fadeIn");
-    $(".web-design img.animated").addClass("slideInUp");
+    $(".web-design img.animated").addClass("slideInUp");    
+});
+
+$(document).ready(function() {
     console.log(this);
     $(this).on("scroll", function(e){
         console.log(this);
         e.preventDefault();
     });
-});
-
-$(document).ready(function() {
     $(".menu a").click(function() {
        ga('send', 'event', 'menu-links', 'Click', $(this).attr("href")); 
     });    
