88d29dcd3b8165e32c0400e30060c7dae2583cef
Henry Maestu <henry.maestu@gmail.com>
2017-02-25 02:32:39 +0200

    test

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     $(".menu a").click(function() {
        ga('send', 'event', 'menu-links', 'Click', $(this).attr("href")); 
-    });    
+    });
 });
 
 $("img").load(function() {    
     $('.uil-ring-css').remove();
 });
-
